b41d4cefaba41eb88e39cfc20fca43a7fbde29a1
Saatvik K <48349076+SaatvikK@users.noreply.github.com>
2022-03-09 20:00:02 +0000

    .

--- controllers.js
+++ controllers.js
@@ -2,18 +2,31 @@ const fs = require("fs");
 
 exports.postUserData = (req, res) => {
   const [usr, pwd] = [req.params.user, req.params.pwd];
-  try { fs.mkdirSync(`./database/users/${usr}`); } catch(e) { res.json({ result: false, reason: "Didn't make database.", err: e}); }
+
+  // Trying to make the collection for the user using mkdirSync().
+  // Also wrapping the function around a try-catch so that the server code doesnt stop execute and we can send the client
+  // an error message.
+
+  try { fs.mkdirSync(`./database/users/${usr}`); } catch(e) { res.status(500); res.json({ result: false, reason: "Unable to create collection folder for new user.", error: e.message, data: null }); }
+  
+  // Here, we are creating the info.json document with the username and password that was sent in the URL request.
+  // Instead of a try-catch, we use a callback function. This function is one that gets executed after fs.writeFile() finishes.
+  // It is equivalent to a try-catch.
   fs.writeFile(`./database/users/${usr}/info.json`, JSON.stringify({ username: usr, password: pwd }), function(err) { 
-    if(err) res.json({ result: false, reason: "unable to create collection" }) 
-    else res.json({ result: true, reason: null }) 
+    // Checking if there is an error. If so, we send an error message to the client along with a HTTP 500 status.
+    if(err) { res.status(500); res.json({ result: false, reason: "Unable to create document for new user.", error: err, data: null }); }
+    // Else, we can assume that the document was created and written to successfully, and so we send a HTTP 200 (OK) success status.
+    else { res.status(200); res.json({ result: true, reason: null, error: null, data: null }); }
   });
 }
 
 exports.postGameData = (req, res) => {
-  function fileErr(e, res, GameID) { res.json({ result: false, message: "Could not make database for game " + GameID + ".", error: e.message, data: null }); console.log(e); }
+  function fileErr(e, res, GameID) { res.status(500); res.json({ result: false, reason: "Could not make database for game.", error: e.message, data: null }); console.log(e); }
   const GameID = req.params.GameID;
-  if(Number.isInteger(parseInt(GameID))) {
+  if(Number.isInteger(parseInt(GameID))) { // GameID must be an integer, so checking if it is.
     try {
+      // Creating the two required collections and all the required documents for the database.
+      // First, each key-value pair for all documents are initialized to `0` (if the value is an int) or "" (if the value is a string)
       fs.mkdirSync(`./database/${GameID}/stats`, { recursive: true });
       fs.writeFile(`./database/${GameID}/stats/score.json`, JSON.stringify({value: 0}), function(err) { if(err) if(err) fileErr(err, res, GameID) });
       fs.writeFile(`./database/${GameID}/stats/wave.json`, JSON.stringify({value: 0}), function(err) { if(err) if(err) fileErr(err, res, GameID) });
@@ -24,56 +37,66 @@ exports.postGameData = (req, res) => {
 
       fs.writeFile(`./database/${GameID}/settings/lives.json`, JSON.stringify({LivesRemaining: 0, TotalLives: 0}), function(err) { if(err) fileErr(err, res, GameID) });
       fs.writeFile(`./database/${GameID}/settings/user.json`, JSON.stringify({user: req.params.user}), function(err) { if(err) fileErr(err, res, GameID) });
-
-      res.json({ result: true, message: "Game's database has been made, use PUT method to update it.", error: null, data: null });
+      
+      // Responding to the client with success.
+      res.status(200);
+      res.json({ result: true, reason: "Game's database has been made, use PUT method to update it.", error: null, data: null });
     } catch(e) {
-      res.json({ message: "Could not make database for game " + GameID + ".", err: e.message});
+      // Responding to the client with an internal server error.
+      res.status(500);
+      res.json({ result: false, reason: "Could not make database for game.", err: e.message, data: null });
     }
 
-  } else { res.json({ message: "GameID needs to be an integer."}); }
+  } else { res.status(400); res.json({ result: false, reason: "GameID needs to be an integer.", error: null, data: null }); }
 }
 
 exports.getLoginInfo = (req, res) => {
   const [usr, pwd] = [req.params.user, req.params.pwd];
   let data;
-  if(fs.existsSync(`./database/users/${usr}/`)) {
-    try { 
+  // Checking if the collection for the requested username exists, i.e. does the user have an account?
+  if(fs.existsSync(`./database/users/${usr}/`)) { // If true
+    try {
+      // Get the data of the document in the collection and compare the database password with the one received in the request.
+      // If the two passwords are the same, the server sends back a success message.
       data = JSON.parse(fs.readFileSync(`./database/users/${usr}/info.json`, "utf8"));
-      if(data.password == pwd) res.json({ result: true, reason: null });
-      else res.json({ result: false, reason: "password incorrect" });
-    } catch(e) { console.log(e); res.json({ reason: "Error while reading JSON file for user.", error: e})}
+      if(data.password == pwd) { res.status(200); res.json({ result: true, reason: "Login successful.", error: null, data: null }); }
+      else { res.status(401); res.json({ result: false, reason: "Password incorrect.", error: null, data: null }); }
+    } catch(e) { console.log(e); res.status(500); res.json({ result: false, reason: "Error while reading JSON file for user.", error: e.message, data: null }); }
 
     
-  } else { res.json({ result: false, reason: "username incorrect" }) }
+  } else { res.json({ result: false, reason: "Username incorrect." }) }
 }
 
 exports.getGameData = (req, res) => {
   const GameID = req.params.GameID;
   if(Number.isInteger(parseInt(GameID)) == false || !fs.existsSync(`./database/${GameID}`)) {
-    res.json({ message: "GameID either was not an integer or game database does not exist." });
+    res.status(400);
+    res.json({ result: false, reason: "GameID either was not an integer or game database does not exist.", error: null, data: null });
     return;
   }
 
   const collection = req.params.collection;
   if(fs.existsSync(`./database/${GameID}/${collection}`) == false) {
-    res.json({ message: "Database for this game is corrupted.", where: "collection" });
+    res.status(404);
+    res.json({ result: false, reason: "Database for this game is corrupted.", where: "collection", error: null, data: null });
     return;
   }
 
   const document = req.params.document;
-  console.log(fs.existsSync(`./database/${GameID}/${collection}/${document}.json`))
   if(fs.existsSync(`./database/${GameID}/${collection}/${document}.json`) == false) {
-    res.json({ message: "Database for this game is corrupted.", where: "document" });
+    res.status(404);
+    res.json({ result: false, reason: "Database for this game is corrupted.", where: "document", error: null, data: null });
   }
 
   try { 
     const data = JSON.parse(fs.readFileSync(`./database/${GameID}/${collection}/${document}.json`, "utf8"));
-    res.json({ GameID: GameID, collection: collection, document: data }); 
+    res.status(200);
+    res.json({ result: true, reason: `Data retrieved from GameID ${GameID} in the ${collection}/${document} document`, error: null, data: data }); 
   }
-  catch(e) { res.json({ message: "Could not read document " + document, error: e.message }); }
+  catch(e) { res.status(500); res.json({ result: false, reason: "Could not read document " + document, error: e.message, data: null }); }
 }
 
-exports.getAllGames = async (req, res) => {
+exports.getAllGames = (req, res) => {
   fs.readdir('./database', { withFileTypes: true }, (error, files) => {
     if (error) throw error;
     const games = files
@@ -89,7 +112,7 @@ exports.getAllGames = async (req, res) => {
         AllGames.push({ GameID: games[i], score: score, difficulty: difficulty, usrn: user });
       }
     }
-    res.json({ result: true, data: AllGames });
+    res.json({ result: true, reason: "eh", error: null, data: AllGames });
   });  
 }
 
@@ -98,29 +121,34 @@ exports.doesGameExist = (req, res) => {
   if(Number.isInteger(parseInt(GameID))) {
     listDBs(req, res);
   } else if(!fs.existsSync(`./database/${GameID}`)) {
+    res.status(200);
     res.json({ DoesGameExist: false });
     return;
   }
+  res.status(200);
   res.json({ DoesGameExist: true });  
 }
 
 exports.putGameData = (req, res) => {
   const GameID = req.params.GameID;
   if(Number.isInteger(parseInt(GameID)) == false || !fs.existsSync(`./database/${GameID}`)) {
-    res.json({ message: "GameID either was not an integer or game database does not exist." });
+    res.status(400);
+    res.json({ result: false, reason: "GameID either was not an integer or game database does not exist.", error: null, data: null });
     return;
   }
 
   const collection = req.params.collection;
   if(fs.existsSync(`./database/${GameID}/${collection}`) == false) {
-    res.json({ message: "Database for this game is corrupted.", where: "collection" });
+    res.status(404);
+    res.json({ result: false, reason: "Database for this game is corrupted.", where: "collection" });
     return;
   }
 
   const document = req.params.document;
   console.log(fs.existsSync(`./database/${GameID}/${collection}/${document}.json`))
   if(fs.existsSync(`./database/${GameID}/${collection}/${document}.json`) == false) {
-    res.json({ message: "Database for this game is corrupted.", where: "document" });
+    res.status(404);
+    res.json({ result: false, reason: "Database for this game is corrupted.", where: "document" });
   }
 
   let [key, data, FilePath] = [req.params.key, req.params.data, `./database/${GameID}/${collection}/${document}`];
@@ -129,10 +157,11 @@ exports.putGameData = (req, res) => {
   if(document == "score" || document == "wave") {
     const DataAsInt = parseInt(data);
     if(Number.isInteger(DataAsInt)) jsonData.value = DataAsInt;
-    else res.json({ message: "Data value must be an integer for any 'score' or 'wave' value."});
+    else { res.status(400); res.json({ result: false, reason: "Data value must be an integer for any 'score' or 'wave' value.", error: null, data: null }); }
 
   } else if(document == "user") {
-    res.json({ message: "Document cannot be the user value as the user value may not be updated."});
+    res.status(400);
+    res.json({ result: false, reason: "Document cannot be the user value as the user value may not be updated.", error: null, data: null });
 
   } else if(document == "difficulty") {
     if(data == "Hard" || data == "Medium" || data == "Easy" || data == "Casual" || data == "Normal") {
@@ -148,12 +177,15 @@ exports.putGameData = (req, res) => {
       jsonData.AlienCooldown = DifficultyRules[data].alien;
       jsonData.PlayerCooldown = DifficultyRules[data].player;
       jsonData.AlienBulletsMax = DifficultyRules[data].AlienBulletsMax;
+    } else {
+      res.status(400);
+      res.json({ result: false, reason: "Difficulty must be 'Hard', 'Medium', 'Easy', or 'Casual/Normal'", error: null, data: null });
     }
 
   } else if(document == "lives") {
     const DataAsInt = parseInt(data);
     if(Number.isInteger(DataAsInt)) jsonData[key] = DataAsInt;
-    else res.json({ message: "Data value must be an integer for any 'lives' value."});
+    else { res.status(400); res.json({ result: false, reason: "Data value must be an integer for any 'lives' value.", error: null, data: null }); }
   }
   //Stringify the dictionary.  JSON.stringify() converts the object/value into a JSON string.
   jsonData = JSON.stringify(jsonData);
@@ -162,9 +194,10 @@ exports.putGameData = (req, res) => {
 
   fs.writeFile(FilePath + ".json", jsonData, function(err) { //function(err) is the callback function
     if(err) {
-      res.json({ message: "Error while updating document.", error: err.message});
+      res.status(500);
+      res.json({ result: false, reason: "Error while updating document.", error: err.message, data: null });
       console.log(err);
-    } else { res.json({ message: "Done."}); }
+    } else { res.status(200); res.json({ result: true, reason: "Updated document.", error: null, data: null }); }
   });
 }
 
@@ -172,8 +205,9 @@ exports.deleteGameData = (req, res) => {
   const GameID = req.params.GameID;
   if(Number.isInteger(parseInt(GameID))) {
     fs.rmSync(`./database/${GameID}`, { recursive: true, force: true });
-    res.json({ message: "Done." })
-  } else { res.json({ message: "GameID needs to be an integer."}); }
+    res.status(200);
+    res.json({ result: true, reason: "Done.", error: null, data: null });
+  } else { res.status(400); res.json({ result: true, reason: "GameID needs to be an integer.", error: null, data: null }); }
 }
 
 
--- index.js
+++ index.js
@@ -1,28 +1,67 @@
+//--------- IMPORT MODULES ---------\\
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 const fs = require("fs");
-
+const MarkdownIt = require('markdown-it');
 const controllers = require("./controllers.js");
+//-----------------------------------\\
+
+//----- Initialize Express Server -----\\
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.set("view engine", "ejs");
+
 
 const version = "v1"
 const BaseRoute = `NEA_API/${version}`;
 //https://www.domain.name.com/NEA_API/[version]/{GameID}/{collection}/{document}/{key}/{data}
 
+// This is the function that handles GET requests for the base route.
+// When a client sends a GET request for a URL with no resource endpoint (i.e. www.domain.name.com), 
+// this is the function that will be triggered.
+// In this case, the server simply responds with the API documentation.
 app.get("/", (req, res) => {
-  res.json({ message: "hai" });
+  const md = new MarkdownIt();
+  const data = fs.readFileSync("README.md", "utf8"); // Reading the documentation file.
+  const style = `
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap');
+    h1 {
+      font-family: 'Montserrat', sans-serif;
+      font-size: 50px;
+    }
+    
+    h3 {
+      font-family: 'Montserrat', sans-serif;
+    }
+    
+    h2 {
+      font-family: 'Montserrat', sans-serif;
+    }
+    
+    p {
+      font-family: 'Ubuntu', sans-serif;
+    }
+    </style>
+  `;
+  res.send(style + md.render(data)); // Rendering the documentation and adding the styles before it.
 });
 
 subRouter(app);
 
+// The server is now listening for requests on the predetermined port. Because this is a HTTP(S) application, the port is 8080.
 app.listen(port);
 
 console.log('RESTful API server started on: ' + port);
 
 function subRouter(app) {
+  // Below are the various endpoints that can be used to request resources from the databse using the API.
+  // Each combination of HTTP method (POST, GET, PUT, DELETE) and URL endpoints must be entirely unique.
+  // Endpoints can be dynamic, to allow the client to add parameters into it. This way, information and data can be easily
+  // sent to the server.
+  // In express, URL/endpoint parameters can be identified using a colon then the parameter name, like declaring a variable.
   app.get("/" + BaseRoute + "/users/:user/:pwd", (req, res) => {
     controllers.getLoginInfo(req, res);
   });
